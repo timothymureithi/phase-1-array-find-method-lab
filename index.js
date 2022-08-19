5701a669e85a7bd34b5598eb6f8d46b6ed3ef78c
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-08-19 04:20:29 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+
+  ]
+
+
+//Function to return super bowl win year
+  function superbowlWin(record){
+    for (let items of record){
+        if(items.result === 'W'){
+            return(items.year)
+        }
+    }
+  }
+
+
+//If there is no win retun undefined
+  let Item = record.find(superbowlWin);
